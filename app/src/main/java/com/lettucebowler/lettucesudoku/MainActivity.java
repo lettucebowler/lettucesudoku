93b09f9380c1225f8993fe3cd40848a6baba9de1
Grant Montgomery <montg433@d.umn.edu>
2020-07-17 13:04:13 -0500

    optimized imports

--- app/src/main/java/com/lettucebowler/lettucesudoku/MainActivity.java
+++ app/src/main/java/com/lettucebowler/lettucesudoku/MainActivity.java
@@ -1,44 +1,16 @@
 package com.lettucebowler.lettucesudoku;
 
-import android.content.Context;
 import android.content.Intent;
-import android.graphics.Color;
-import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
-import android.provider.MediaStore;
-import android.util.AttributeSet;
-import android.util.TypedValue;
-import android.view.Gravity;
-import android.view.View;
 import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.SeekBar;
-import android.widget.TableLayout;
-import android.widget.TableRow;
-import android.widget.Toast;
 import android.widget.ToggleButton;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.view.ContextThemeWrapper;
-import androidx.gridlayout.widget.GridLayout;
-
-import com.lettucebowler.lettucesudoku.domains.sudoku.Sudoku;
-import com.lettucebowler.lettucesudoku.domains.sudoku.SudokuProblem;
-import com.lettucebowler.lettucesudoku.domains.sudoku.SudokuState;
-import com.lettucebowler.lettucesudoku.framework.problem.SolvingAssistant;
-
-import java.util.ArrayList;
-import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
-
-    private int order;
-    private int num_starter_hints;
-    private boolean game_has_been_started;
-    private boolean highlight_adjacent;
-    private boolean highlight_all_num;
-    private boolean color_text_on_correctness;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
